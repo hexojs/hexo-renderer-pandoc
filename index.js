d1f8f72db50195fc1e4a6e6cfbf953564f1ae5d5
Richard Yan <richardyan314@foxmail.com>
2019-02-08 10:49:27 -0500

    tabs to spaces

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var pandocRenderer = function(data, options, callback){
         extensions += extension;
       });
     }
-	  
+
     if(config.filters) {
       config.filters.forEach(function(filter) {
         filters.push('--filter');
@@ -20,13 +20,13 @@ var pandocRenderer = function(data, options, callback){
 
     if(config.extra) {
       for(var e in config.extra) {
-	    var eoption = config.extra[e];
+        var eoption = config.extra[e];
         for (var key in eoption){
           extra.push('--' + key);
           if(eoption[key]!=null) {
             extra.push(eoption[key]); 
           }
-	}
+        }
       }
     }
 
@@ -54,49 +54,49 @@ var pandocRenderer = function(data, options, callback){
   .concat(filters)
   .concat(extra)
   .concat(meta);
-	
+  
   if(config && config.template) args.push("--template=" + config.template);
 
-	var src = data.text.toString();
+  var src = data.text.toString();
 
-	var pandoc = spawn('pandoc', args);
+  var pandoc = spawn('pandoc', args);
 
-	var result = '';
-	var error = '';
+  var result = '';
+  var error = '';
 
-	pandoc.stdout.setEncoding('utf8');
+  pandoc.stdout.setEncoding('utf8');
 
-	pandoc.stdout.on('data', function (data) {
-		result += data.toString();
-	});
+  pandoc.stdout.on('data', function (data) {
+    result += data.toString();
+  });
 
-	pandoc.stderr.on('data', function (data) {
-		error += data.toString();
-	});
+  pandoc.stderr.on('data', function (data) {
+    error += data.toString();
+  });
 
-	pandoc.stdin.write(src, 'utf8');
+  pandoc.stdin.write(src, 'utf8');
 
-	pandoc.on('close', function (code, signal) {
-		var msg = '';
-		if (code !== 0)
-			msg += 'pandoc exited with code '+code+(error ? ': ' : '.');
-		// remove these two harmless warning messages
-		// to prevent program from crashing
-		error = error.replace(
-			"[WARNING] This document format requires a nonempty <title> element.\r\n  Please specify either 'title' or 'pagetitle' in the metadata.\r\n  Falling back to 'Untitled'\r\n"
-		,'');
-		error = error.replace(
-			"[WARNING] This document format requires a nonempty <title> element.\r\n  Please specify either 'title' or 'pagetitle' in the metadata,\r\n  e.g. by using --metadata pagetitle=\"...\" on the command line.\r\n  Falling back to 'Untitled'\r\n"
-		,'')
-		if (error)
-			msg += error;
-		if (msg)
-			return callback(new Error(msg));
-		else{
-			if (result === '') console.log("The next file error: ");
-			callback(null, result);
-		}
-	});
+  pandoc.on('close', function (code, signal) {
+    var msg = '';
+    if (code !== 0)
+      msg += 'pandoc exited with code '+code+(error ? ': ' : '.');
+    // remove these two harmless warning messages
+    // to prevent program from crashing
+    error = error.replace(
+      "[WARNING] This document format requires a nonempty <title> element.\r\n  Please specify either 'title' or 'pagetitle' in the metadata.\r\n  Falling back to 'Untitled'\r\n"
+    ,'');
+    error = error.replace(
+      "[WARNING] This document format requires a nonempty <title> element.\r\n  Please specify either 'title' or 'pagetitle' in the metadata,\r\n  e.g. by using --metadata pagetitle=\"...\" on the command line.\r\n  Falling back to 'Untitled'\r\n"
+    ,'')
+    if (error)
+      msg += error;
+    if (msg)
+      return callback(new Error(msg));
+    else{
+      if (result === '') console.log("The next file error: ");
+      callback(null, result);
+    }
+  });
 
     pandoc.stdin.end();
 
